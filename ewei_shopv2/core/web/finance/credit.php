6b2c7106d3bee0dd9df623a2dcc2989334327e5a
hwkkevin <30752149+hwkkevin@users.noreply.github.com>
2017-10-27 20:52:24 +0800

    第三版20171027

--- ewei_shopv2/core/web/finance/credit.php
+++ ewei_shopv2/core/web/finance/credit.php
@@ -40,7 +40,7 @@ class Credit_EweiShopV2Page extends WebPage
 		{
 			$condition .= ' and m.groupid=' . intval($_GPC['groupid']);
 		}
-		$sql = 'select log.*,m.id as mid, m.realname,m.avatar,m.nickname,m.avatar, m.mobile, m.weixin,u.username from ' . tablename('mc_credits_record') . ' log ' . ' left join ' . tablename('users') . ' u on log.operator<>0 and log.operator<>log.uid and  log.operator=u.uid' . ' left join ' . tablename('ewei_shop_member') . ' m on m.uid=log.uid' . ' left join ' . tablename('ewei_shop_member_group') . ' g on m.groupid=g.id' . ' left join ' . tablename('ewei_shop_member_level') . ' l on m.level =l.id' . ' where 1 ' . $condition . ' ORDER BY log.createtime DESC ';
+		$sql = 'select log.*,m.id as mid, m.realname,m.avatar,m.nickname,m.avatar, m.mobile, m.weixin,u.username from ' . tablename('mc_credits_record') . ' log ' . ' left join ' . tablename('users') . ' u on log.operator<>0 and log.operator<>log.uid and  log.operator=u.uid' . ' left join ' . tablename('ewei_shop_member') . ' m on m.openid=log.openid' . ' left join ' . tablename('ewei_shop_member_group') . ' g on m.groupid=g.id' . ' left join ' . tablename('ewei_shop_member_level') . ' l on m.level =l.id' . ' where 1 ' . $condition . ' ORDER BY log.createtime DESC ';
 		if (empty($_GPC['export'])) 
 		{
 			$sql .= 'LIMIT ' . (($pindex - 1) * $psize) . ',' . $psize;
